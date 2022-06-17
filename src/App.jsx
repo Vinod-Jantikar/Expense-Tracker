35e33956d162d79b011c3186af128294bf2b4e89
Vinod Jantikar <vinodjantikar999@gmail.com>
2022-06-17 17:09:08 +0530

    Committed full Project

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import AddTransaction from "./components/AddTransaction";
+import Balance from "./components/Balance";
+import Header from "./components/Header";
+import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
+import { GlobalContextProvider } from "./context/GlobalState";
+
+function App() {
+  return (
+    <GlobalContextProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalContextProvider>
+  );
+}
+
+export default App;
